146f88de01f53df8ac05ba109357cbf28bb037ca
Vladik <judosportage@gmail.com>
2020-02-24 19:08:02 +0200

    WorkInClass

---
+++ Header.h
@@ -0,0 +1,18 @@
+#ifndef CLASSWORK_HEADER_H
+#define CLASSWORK_HEADER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+class Hash
+{
+public:
+    virtual void add(std::string& value) = 0;
+    virtual bool check(std::string& value) = 0;
+    virtual int getMaxCollisions() const = 0;
+};
+
+#endif //CLASSWORK_HEADER_H
